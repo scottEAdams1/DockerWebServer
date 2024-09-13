9cc3e1fff6ad52391cb38ed05592345df759e9b2
scottEAdams1 <scott.e.adams01@gmail.com>
2024-09-14 00:08:11 +0100

    Updated main.go

--- main.go
+++ main.go
@@ -13,18 +13,21 @@ func main() {
 
 	m.HandleFunc("/", handlePage)
 
-	const addr = ":8080"
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default to port 8080
+	}
 	srv := http.Server{
 		Handler:      m,
-		Addr:         port,
+		Addr:         ":" + port, // construct the address
 		WriteTimeout: 30 * time.Second,
 		ReadTimeout:  30 * time.Second,
 	}
 
 	// this blocks forever, until the server
 	// has an unrecoverable error
-	fmt.Println("server started on ", port)
+	//fmt.Println("server started on ", port)
+	fmt.Println("Server will start on ", srv.Addr)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
 }
@@ -35,7 +38,7 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	const page = `<html>
 <head></head>
 <body>
-	<p> Hello from Docker! I'm a Go server. </p>
+	<p> Hi Docker, I pushed a new version! </p>
 </body>
 </html>
 `
