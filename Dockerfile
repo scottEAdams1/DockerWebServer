e4f7ce8cdbaff1640619d9aa2dd960b8d17b77b4
scottEAdams1 <scott.e.adams01@gmail.com>
2024-09-13 23:22:11 +0100

    Added env

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM debian:stable-slim
 COPY DockerWebServer /bin/DockerWebServer
+ENV PORT=8080
 CMD ["/bin/DockerWebServer"]
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -13,16 +14,17 @@ func main() {
 	m.HandleFunc("/", handlePage)
 
 	const addr = ":8080"
+	port := os.Getenv("PORT")
 	srv := http.Server{
 		Handler:      m,
-		Addr:         addr,
+		Addr:         port,
 		WriteTimeout: 30 * time.Second,
 		ReadTimeout:  30 * time.Second,
 	}
 
 	// this blocks forever, until the server
 	// has an unrecoverable error
-	fmt.Println("server started on ", addr)
+	fmt.Println("server started on ", port)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
 }
